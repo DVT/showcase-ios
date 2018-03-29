124375c04c082201c176d272ebf3b620f7e49499
Kagiso Mohajane <kmohajane@jhb.dvt.co.za>
2018-03-29 14:34:18 +0200

    Adding presentation extension for app detail screen

--- DVTShowcase/UIViewControllerExtension.swift
+++ DVTShowcase/UIViewControllerExtension.swift
@@ -1,9 +1,23 @@
-//
-//  UIViewControllerExtension.swift
-//  DVTShowcase
-//
-//  Created by KG's_Mac on 2018/03/09.
-//  Copyright © 2018 DVT. All rights reserved.
-//
 
-import Foundation
+import UIKit
+
+extension UIViewController {
+    
+    // MARK: - View Controller Presentation
+    
+    func present(viewController: UIViewController, withTransition: Bool = false) {
+        let closeButton = UIBarButtonItem(barButtonSystemItem: .stop, target: self, action: #selector(dismissPresentedViewController))
+        viewController.navigationItem.leftBarButtonItem = closeButton
+        let navigationController = UINavigationController(rootViewController: viewController)
+        present(navigationController: navigationController)
+    }
+    
+    private func present(navigationController: UINavigationController) {
+        navigationController.modalPresentationStyle = .formSheet
+        navigationController.modalTransitionStyle = .coverVertical
+        present(navigationController, animated: true, completion: nil)
+    }
+    @objc func dismissPresentedViewController() {
+            dismiss(animated: true, completion: nil)
+    }
+}
